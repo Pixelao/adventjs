73ff62c4d608a2ba3b64337e4bb696bdfb1156d1
Adrián Martín <contacto@adrianmr.com>
2022-12-06 12:09:12 +0100

    ✨ Removed unnecessary code

--- 2022/05.js
+++ 2022/05.js
@@ -79,12 +79,8 @@ function getMaxGifts(giftsCities, maxGifts, maxCities) {
   5 - Devolvemos el resultado que más regalos sume
   */
   return Math.max(
-    ...[
-      ...giftsCities.reduce(
-        (x, y) => x.concat(x.map((x) => [y].concat(x))),
-        [[]]
-      ),
-    ]
+    ...giftsCities
+      .reduce((x, y) => x.concat(x.map((x) => [y].concat(x))), [[]])
       .filter((x) => x.length <= maxCities)
       .map((x) => x.reduce((a, b) => a + b, 0))
       .filter((x) => x <= maxGifts)
